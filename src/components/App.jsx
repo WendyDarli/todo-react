d68b8faf2cda2d8366132581fd22dfad4d3b5cdd
WendyDarli <end.y.darli@gmail.com>
2025-06-19 20:43:56 -0300

    feat(Modal): add component layout and styles
    Modal for creating/updating tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import Header from './Header/Header.jsx'
 import Wrapper from './Wrapper/Wrapper.jsx'
-import Todo from './Todo/Todo.jsx'
-
+//import Todo from './Todo/Todo.jsx'
+import Modal from './Create-Delete-Modal/Modal.jsx'
 function App() { //root
 
     return(
@@ -9,7 +9,7 @@ function App() { //root
         <Header/>
         <div id='centralizer'>
           <Wrapper/>
-          <Todo/>
+          <Modal/>
         </div>
       </>      
     );
