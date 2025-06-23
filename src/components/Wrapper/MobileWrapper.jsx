aacc7791f272c15a02db1191319da1e00d6c2e67
WendyDarli <end.y.darli@gmail.com>
2025-06-23 17:09:28 -0300

    feat(MobileWrapper)Add component layout and style

---
+++ src/components/Wrapper/MobileWrapper.jsx
@@ -0,0 +1,22 @@
+import './Wrapper.css';
+import './MobileWrapper.css';
+
+function MobileWrapper(){
+    return(
+        <div id='wrapper'>
+            <div id='top-wrapper'>
+                <div className="lilac-container"><p><b>All Tasks</b></p></div>
+                
+            </div>
+
+            <div id='bottom-wrapper'>
+                <button className='border'>Filter</button>
+                <button className='border short-btn'>New +</button> 
+            </div>
+
+        </div>
+
+    )
+}
+
+export default MobileWrapper
