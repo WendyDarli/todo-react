83243cb0084d25f075e44155937ef818bbac38d7
WendyDarli <end.y.darli@gmail.com>
2025-07-16 17:56:03 -0300

    Group tasks by due date and improve date display
    Tasks are now grouped and displayed by their due date in descending order, with formatted labels such as 'Today', 'Tomorrow', and 'Yesterday'
    Added date-fns as a dependency, updated Todo component logic and styles

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,29 +1,62 @@
+import React from 'react';
 import './Todo.css'
+import { format, isToday, isTomorrow, isYesterday, parseISO } from 'date-fns';
 
 function Todo({ userTasks, openModal }){
+
+    //groups tasks by date in desc order
+    const groupedTasks = Object.entries(
+        userTasks.reduce((acc, task) => {
+            const date = task.dueDate;
+
+            if(!acc[date]){
+                acc[date] = [];
+            };
+
+            acc[date].push(task);
+            return acc;
+        }, {})
+    ).sort(([a], [b]) => new Date(b) - new Date(a))
+    
+
+    //format date to display in the UI
+    function formatDate(dateString) {
+        const date = parseISO(dateString); 
+
+        if (isToday(date)) return 'Today';
+        if (isTomorrow(date)) return 'Tomorrow';
+        if (isYesterday(date)) return 'Yesterday';
+
+        return format(date, 'MMM d');
+    }
+
+
     return(
         <>
-        {/*TODO: add conditional rendering for status background color*/}
-        {/*TODO: add date p that groups todos inside this due date*/}
-        {userTasks.map((task) => (
-            <div className='todo-container light-border' key={task.id} onClick={() => {openModal('edit', task.id)}}>
-                <div>
-                    <p className='todo-name'>{task.todoName}</p>
-                    <p className='todo-description'>{task.description}</p>
-                </div>
-                <div >
-                    <p className={`todo-status ${ 
-                        task.status === 'To-Do' ? 'red' : 
-                        task.status === 'In-Progress' ? 'yellow' : 
-                        'green'
-                    }`}>{task.status}</p>
-                </div>
+            {groupedTasks.map(([dueDate, tasks]) => (
+                <React.Fragment key={dueDate}>
+                    <p className='p-date'>{formatDate(dueDate)}</p> 
+                    {tasks.map((task) => (
+                        <div className='todo-container light-border' key={task.id} onClick={() => {openModal('edit', task.id)}}>
+                            <div>
+                                <p className='todo-name'>{task.todoName}</p>
+                                <p className='todo-description'>{task.description}</p>
+                            </div>
+                            <div >
+                                <p className={`todo-status ${ 
+                                    task.status === 'To-Do' ? 'red' : 
+                                    task.status === 'In-Progress' ? 'yellow' : 
+                                    'green'
+                                }`}>{task.status}</p>
+                            </div>
 
-            </div>
-            
-        ))}
-    </>
+                        </div>
+                    
+                    ))}                
+                </React.Fragment>  
+            ))}
+        </>
     );
 }
 
-export default Todo
+export default Todo;
