1b50961ddb2e1fab6c9c73ca83923479339292d0
WendyDarli <w.end.darli@gmail.com>
2025-07-30 12:06:27 -0300

    Update Header theme icon paths

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import  { useTheme }  from '../../hooks/useTheme';
 
 function Header(){
     const { theme, toggleTheme } = useTheme();
-    const bgImage = theme === 'light' ? 'url(../svgs/dark-mode.svg)' : 'url(../svgs/light-mode.svg)';
+    const bgImage = theme === 'light' ? 'url(/todo-react/svgs/dark-mode.svg)' : 'url(/todo-react/svgs/light-mode.svg)';
     return(
         <div id="header">
             <h1>To do app</h1>
