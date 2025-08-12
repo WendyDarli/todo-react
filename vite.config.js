604707530a16ed6ae47aca62238521bd88351e3a
WendyDarli <w.end.darli@gmail.com>
2025-08-12 14:29:55 -0300

    test: initial setup for testing environment and dependencies
    installed vitest and jsdom for the testing environment
    installed testing library react for rendering and interacting with components
    installed user-event to simulate user interactions
    installed jest-dom for custom matchers

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/todo-react/', 
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
 })
